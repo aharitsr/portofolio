bc59399de27dc6ca0f443492e1cc059a841962c6
Ahmad Harits Ramadhani <87635224+aharitsr@users.noreply.github.com>
2026-08-02 23:53:12 +0700

    contact me

--- src/components/section/contact-section.tsx
+++ src/components/section/contact-section.tsx
@@ -31,10 +31,8 @@ export default function ContactSection() {
             rel="noopener noreferrer"
             className="text-blue-500 hover:underline underline-offset-4 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 rounded-sm"
           >
-            with a direct question on twitter
-          </Link>{" "}
-          and I&apos;ll respond whenever I can. I will ignore all
-          soliciting.
+            with a direct question on Gmail
+          ahharits.r@gmail.com thank you!
         </p>
       </div>
     </div>
